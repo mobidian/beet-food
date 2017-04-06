6679b1ee52b27375e59bee530f05c3160a48f95f
Yurii Svystun <svystun@gmail.com>
2017-04-06 19:46:34 +0300

    Added Models folder and User class.

--- app/Controllers/Auth/RegisterController.php
+++ app/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
